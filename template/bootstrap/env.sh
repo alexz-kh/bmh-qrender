da947611ebeda801c184f72f20348faf49ffcd33
azvyagintsev <azvyagintsev@mirantis.com>
2019-09-10 17:19:12 +0300

    upd

--- template/bootstrap/env.sh
+++ template/bootstrap/env.sh
@@ -1,12 +1,24 @@
-BS_DIR="${HOME}/bootstrap"
-export DEBUG="true"
-export KAAS_BM_AIO_VALUES="${BS_DIR}values_ironic_aio.yaml"
-export KAAS_BM_METAL3_VALUES="${BS_DIR}/values_metal3.yaml"
-export KAAS_BM_ENABLED="true"
+#!/bin/bash
 
-export KAAS_BM_PXE_BRIDGE="{{ cookiecutter.seed_host.bridge }}"
+export BS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export DEBUG="true"
+export KAAS_BM_ENABLED="true"
+#
+export KAAS_BM_AIO_VALUES="${BS_DIR}/values_ironic_aio.yaml"
+export KAAS_BM_AIO_MGMT_VALUES="${BS_DIR}/mgmt_values_ironic_aio.yaml"
+#
+export KAAS_BM_METAL3_VALUES="${BS_DIR}/values_metal3.yaml"
+export KAAS_BM_METAL3_MGMT_VALUES="${BS_DIR}/mgmt_values_metal3.yaml"
+#
 export KAAS_BM_PXE_IP="{{cookiecutter.pxe.provisioning_ip }}"
+export KAAS_BM_PXE_BRIDGE="{{ cookiecutter.seed_host.bridge }}"
 export KAAS_BM_PXE_MASK="{{ cookiecutter.pxe.provisioning_mask}}"
+export KAAS_BM_LB_HOST="{{ cookiecutter.metallb.host }}"
+#
+export KAAS_BM_METALLB_MGMT_VALUES="${BS_DIR}/mgmt_values_metallb.yaml"
+export KAAS_BM_MACHINES="${BS_DIR}/machine/all.yaml"
+export KAAS_BM_HW="${BS_DIR}/hw/all.yaml"
+#
 export MASTER_FLOATING_IP="{{ cookiecutter.nodes.master.n0.dhcp_ip }}"
 export KEYCLOAK_FLOATING_IP="{{ cookiecutter.nodes.master.n0.dhcp_ip }}"
 export PROXY_FLOATING_IP="127.0.0.1"
@@ -14,38 +26,22 @@ export IAM_FLOATING_IP="{{ cookiecutter.nodes.master.n0.dhcp_ip }}"
 # IPs after pivoting
 export KAAS_BM_MGMT_IP="{{ cookiecutter.nodes.master.n0.dhcp_ip }}"
 
-
-PATH=${PATH}:./bin/:./1/
-export KUBECTL_CMD="${HOME}/cluster-api-provider-openstack/bootstrap/bin/kubectl"
-
-function stubber{
-
-time ./1/clusterctl bootstrap create \
-  --metallb-chart-version 0.9.7 \
-  --metallb-values ${HOME}/bootstrap/values_metallb.yaml \
-  --metallb-mgmt-values ${HOME}/bootstrap/values_metallb.yaml \
-  --ironic-values ${HOME}/bootstrap/values_ironic_aio.yaml \
-  --ironic-mgmt-values ${HOME}/bootstrap/mgmt_values_ironic_aio.yaml \
-  --ironic-chart-version 0.2.0 \
-  --metal3-chart-version 0.2.0 \
-  --metal3-mgmt-values ${HOME}/bootstrap/mgmt_values_metal3.yaml \
-  --metal3-values ${HOME}/bootstrap/values_metal3.yaml \
-  --baremetalhosts ${HOME}/bootstrap/hw/all.yaml \
-  --provider baremetal \
-  --bootstrap-cluster-name clusterapi \
-  --os-cloud openstack \
-  --keyname '' \
-  --external-network-id '' \
-  --cluster-name test1 \
-  --artifactory-repo dev \
-  --chart-repo https://artifactory.mcp.mirantis.net/helm-dev-virtual \
-  --version $(cat .version | awk '{print $1}') \
-  --iam-registration-url 10.0.0.23:8082 \
-  --cluster ${HOME}/bootstrap/cluster.yaml \
-  --machines ${HOME}/bootstrap/machine/all.yaml \
-  --release-path $CAPO_DIR/bootstrap/1/releases/ \
-  --chart-dir $CAPO_DIR/charts/ \
-  --v 4
-
-
+function stubber(){
+for n in 0 1 2 3; do
+  sudo virsh destroy worker-${n} || true
+done
+set -x
+mkdir -p $BS_DIR/dev
+pushd $BS_DIR/dev/
+if [[ ! -f bs.tar.gz ]]; then
+ wget https://artifactory.mcp.mirantis.net/binary-dev-local/kaas/bootstrap-linux-0.2.19-39-608292f.tar.gz -O bs.tar.gz
+ tar -xzf bootstrap.tar.gz
+fi
+HOME=${BS_DIR}/dev/
+export PATH=${PATH}:./bin/:./dev/
+export KUBECTL_CMD="${BS_DIR}/dev/bin/kubectl"
+./bin/kind delete cluster --name=clusterapi  || true
+./bootstrap_new.sh all
+popd
+set +x
 }
